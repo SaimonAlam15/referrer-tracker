c71480f1adcba1b085dcf35bf10fbe1ebc379b34
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-10-02 23:51:47 +0600

    Revert to original param values; Add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Referrer Finder
+
+## Setup Instructions
+
+1. **Create a local `.env` file**  
+    Containing values for all the variables used in `db.py`.
+
+2. **Ensure Python Environment**  
+    Make sure you're on a Python environment running Python version >= 3.10.
+
+3. **Install Dependencies**  
+    ```sh
+    pip install -r requirements.txt
+    ```
+
+4. **Run the App**  
+    ```sh
+    streamlit run main.py
+    ```
--- model_generic_rforest.py
+++ model_generic_rforest.py
@@ -53,14 +53,11 @@ def process_data(input_df):
 
 
 def train_model(X_train, y_train):
-    # params = {
-    #     'criterion': 'gini', 'max_depth': 20, 'min_samples_leaf': 1,
-    #     'min_samples_split': 5, 'n_estimators': 1000
-    # }
-    # reg = RandomForestClassifier(**params)
-    params = {'n_estimators': 10, 'min_samples_split': 10, 'min_samples_leaf': 2, 'max_depth': 5, 'criterion': 'gini'}
-    # Initialize the RandomForestClassifier with the new parameters
-    reg = RandomForestClassifier(**params, n_jobs=1)
+    params = {
+        'criterion': 'gini', 'max_depth': 20, 'min_samples_leaf': 1,
+        'min_samples_split': 5, 'n_estimators': 1000
+    }
+    reg = RandomForestClassifier(**params)
     reg.fit(X_train, y_train)
     return reg
 
