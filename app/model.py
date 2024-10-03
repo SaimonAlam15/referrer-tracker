a8c019e9e31f3672039f37ec38d4ffc7483103a3
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-10-03 17:22:10 +0600

    Reduce to all time low

--- app/model.py
+++ app/model.py
@@ -29,7 +29,7 @@ def train_model(X_train, y_train):
         'criterion': 'gini', 'max_depth': 20, 'min_samples_leaf': 1,
         'min_samples_split': 5, 'n_estimators': 1000
     }
-    reg = RandomForestClassifier(n_estimators=100, random_state = 0)
+    reg = RandomForestClassifier(n_estimators=10, random_state = 0)
     reg.fit(X_train, y_train)
     return reg
 
