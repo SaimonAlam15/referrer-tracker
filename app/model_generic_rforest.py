f43ce1d2e3b543a0cd564a44566d6c8d43a35eda
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-10-07 19:06:38 +0600

    Optimization attempt 4

--- app/model_generic_rforest.py
+++ app/model_generic_rforest.py
@@ -13,7 +13,6 @@ def encode_data(input_df):
     filtered_df = input_df[input_df['TARGET'] == 1]
     feature_names = []
     for col in categorical_columns:
-        cols = []
         if col == 'FIELD_OF_EXPERTISE':
             value_counts = filtered_df['FIELD_OF_EXPERTISE'].str.split(',').explode().value_counts().nlargest(10)
             cols = [f'FIELD_OF_EXPERTISE_{val}' for val in value_counts.index]
@@ -23,20 +22,17 @@ def encode_data(input_df):
         feature_names.extend(cols)
 
     expertise_df = input_df['FIELD_OF_EXPERTISE'].str.get_dummies(sep=",").add_prefix('FIELD_OF_EXPERTISE_')
-    input_df = pd.concat([input_df.drop('FIELD_OF_EXPERTISE', axis=1), expertise_df], axis=1)
-
-    del expertise_df
+    input_df = pd.concat([input_df, expertise_df], axis=1)
     
     new_df = pd.get_dummies(input_df, columns=categorical_columns[:-1])
     
-    df_merged = pd.concat([input_df, new_df]).drop_duplicates(['ID'], keep='last')
+    input_df = pd.concat([input_df, new_df]).drop_duplicates(['ID'], keep='last')
 
-    del new_df, filtered_df
     
     # Drop the original categorical columns
-    df_merged = df_merged.drop(categorical_columns[:-1], axis=1)
+    input_df = input_df.drop(categorical_columns, axis=1)
 
-    return df_merged, feature_names
+    return input_df, feature_names
 
 
 def process_data(input_df):
