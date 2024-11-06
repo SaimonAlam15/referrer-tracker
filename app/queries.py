9b81f43fe6b6bf0f7256c845b8a9643783aa5f54
Saimon Alam <saimonalam@Saimons-MacBook-Pro.local>
2024-11-06 16:10:18 +0600

    Add a filter table above the predicted table

--- app/queries.py
+++ app/queries.py
@@ -115,54 +115,31 @@ MODEL_2_TRAINING_DATA_QUERY = """
 
 
 TEST_DATA_QUERY = """
-        with registered_referrers as (
         select
-            ref.ID,
-            can.EMAIL as CANDIDATE_EMAIL
-        from IREFERRALS ref
-        join CANDIDATES can 
-            on ref.CANDIDATE_ID = can.ID
-        where CANDIDATE_EMAIL is null
-        ),
-        guest_referrers as (
-        select
-            ID,
-            CANDIDATE_EMAIL
-        from IREFERRALS
-        where CANDIDATE_EMAIL is not null
-        ),
-        referrers as (
-        select distinct CANDIDATE_EMAIL from guest_referrers
-        UNION
-        select distinct CANDIDATE_EMAIL from registered_referrers
+        ID::string as ID,
+        EMAIL,
+        FIRST_NAME,
+        LAST_NAME,
+        CITY,
+        STATE,
+        SOURCE,
+        CAREER_LEVEL,
+        TITLE_OF_LAST_POSITION,
+        FIELD_OF_EXPERTISE,
+        (
+            case when DATEDIFF(day, last_activity_time, current_date()) <= 60 then 1.0::float
+            else 0.0::float
+            end
         )
-        
-        select
-            c.ID,
-            c.EMAIL,
-            c.FIRST_NAME,
-            c.LAST_NAME,
-            c.SOURCE,
-            c.CAREER_LEVEL,
-            c.TITLE_OF_LAST_POSITION,
-            c.FIELD_OF_EXPERTISE,
-            c.COUNTRY as candidate_country,
-            c.STATE as candidate_state,
-            c.CITY as candidate_city,
-            (
-                case when DATEDIFF(day, c.last_activity_time, current_date()) <= 60 then 1.0::float
-                else 0.0::float
-                end
-            )
-            as RECENTLY_ACTIVE,
-            (
-                case when r.CANDIDATE_EMAIL is null then 0.0::float
-                else 1.0::float
-                end
-            ) as TARGET
-        from referrers r
-        right join Candidates c
-            on r.CANDIDATE_EMAIL = c.EMAIL
+        as RECENTLY_ACTIVE,
+        (
+            case when ID in (select CANDIDATEID from IREFERRALS)
+            or EMAIL in (select CANDIDATE_EMAIL from IREFERRALS)
+            then 1.0::float
+            else 0.0::float
+            end
+        ) as TARGET
+    from Candidates
     """
 
 
